d2423c8a54eab2ce19b77a441d8418ed6700f764
unknown <anubhav.kaushik@midastravel.org>
2023-12-06 20:21:53 +0530

    orderlist

--- components/Airlines.js
+++ components/Airlines.js
@@ -6,20 +6,20 @@ const Airlines = () => {
     <div className='container mt-5'>
 
           <div className="container airports-grid mb-5">
-     
-            <ul className='row airlines-list-order'>
-            <h2>
+          <h2>
           Other Popular <span className="different-color">Airlines</span>
           </h2>
-            <li className="col-md-2 mt-4 airlines-list ">Lufthansa</li>
+            <ul className='row airlines-list-order'>
+         
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4  ">Lufthansa</li>
         
-            <li className="col-md-2 mt-4 airlines-list">British Airways</li>
-            <li className="col-md-2 mt-4 airlines-list">Westjet</li>
-            <li className="col-md-2 mt-4 airlines-list">Frontier airlines</li>
-            <li className="col-md-2 mt-4 airlines-list">Turkish airlines</li>
-            <li className="col-md-2 mt-4 airlines-list">Finnair</li>
-            <li className="col-md-2 mt-4 airlines-list">Iberia</li>
-            <li className="col-md-2 mt-4 airlines-list">Klm royal dutch airlines</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">British Airways</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Westjet</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Frontier airlines</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Turkish airlines</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Finnair</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Iberia</li>
+            <li className="col-lg-2 col-md-2 col-sm-3 col-xs-6 mt-4 ">Klm royal dutch airlines</li>
             </ul>
             </div>
         
