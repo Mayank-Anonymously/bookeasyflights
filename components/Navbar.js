16e16f190398629c596fcc98a15ebd2b149399bb
anubhav-midas <124804023+anubhav-midas@users.noreply.github.com>
2023-12-11 19:07:59 +0530

    img footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -55,3 +55,67 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+// import Link from "next/link";
+
+// const Navbar = () => {
+//   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+//   const [isActive, setActive] = useState(false);
+//   function handleClick() {
+//     setMobileNavOpen(!isMobileNavOpen);
+//     setActive(!isActive);
+//   }
+//   return (
+//     <>
+//       <div class="navwrap">
+//         <nav class="navbar">
+//           <Link href="/">
+//           <img src="../../../images/logo.png" className="logo" alt="Logo" />
+//           </Link>
+//           <div
+//             class={`menu-toggle ${isActive ? "is-active" : ""}`}
+//             id="mobile-menu"
+//             onClick={handleClick}
+//           >
+//             <span class="bar"></span>
+//             <span class="bar"></span>
+//             <span class="bar"></span>
+//           </div>
+//           <ul class={`nav ${isMobileNavOpen ? "mobile-nav" : ""}`}>
+//           <li className="nav-item">
+//                  <Link className="nav-link" href="/">
+//                    Home
+//                  </Link>
+//                </li>
+//                <li className="nav-item">
+//                  <Link className="nav-link" href="/about">
+//                    About Us
+//                  </Link>
+//                </li>
+//                <li className="nav-item">
+//                  <Link className="nav-link" href="/">
+//                    Travel
+//                  </Link>
+//                </li>
+//                <li className="nav-item">
+//                  <Link className="nav-link" href="/contact">
+//                    Contact Us
+//                  </Link>
+//                </li>
+//           </ul>
+//           <div className="mob_toll d-sm-block d-none">
+//             <span>Call for 24/7 Support</span>
+//             <a href="tel:+1-8888-750-118"> +1-8888-750-118</a>
+//           </div>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
